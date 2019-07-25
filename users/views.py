b554bcf8fda517aa68b8f147a766d1404bd4081d
Batuhan Aydın <batuhanaydin501@gmail.com>
2019-07-25 11:47:46 +0300

    css-login-register duzenlemesi

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
 # Create your views here.
+def register(request):
+    return render(request, 'user/register.html')
+
+def login(request):
+    return render(request, 'user/login.html')    

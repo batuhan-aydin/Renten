48f616947125e348c31bb7b7f30ec33bd0799e7e
Batuhan Aydın <batuhanaydin501@gmail.com>
2019-07-26 16:29:10 +0300

    kategori ayarlari yapildi hem model hem template icinde

--- items/models.py
+++ items/models.py
@@ -2,7 +2,30 @@ from django.db import models
 from django.conf import settings
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)   
+    COLORS = (
+        ('S', 'success'),
+        ('P', 'primary'),
+        ('I', 'info'),
+        ('W', 'warning'),
+        ('D', 'danger'),
+        ('A', 'dark'),
+        ('E', 'secondary'),
+        ('L', 'light')
+    )
+
+    ICONS = (
+        ('T', 'mobile-alt'),
+        ('B', 'book'),
+        ('M', 'music'),
+        ('C', 'fa-car'),
+        ('H', 'home'),
+        ('A', 'basketball-ball'),
+        ('E', 'headphones'),
+        ('D', 'baby-carriage'),
+    )
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=1, choices=COLORS, default='P')
+    icon = models.CharField(max_length=1, choices=ICONS, default='B')
 
 
 class Item(models.Model):
